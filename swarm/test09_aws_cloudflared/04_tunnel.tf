1ec1ec404ffe0c559b7edd432b484b48546e624a
kimgura <oli999@naver.com>
2026-06-11 10:15:29 +0900

    swarm/test09 에 tunnel.tf 에 sample 코드 추가함

--- swarm/test09_aws_cloudflared/04_tunnel.tf
+++ swarm/test09_aws_cloudflared/04_tunnel.tf
@@ -9,6 +9,7 @@ resource "cloudflare_tunnel" "vmware_tunnel" {
   secret     = base64encode(random_password.tunnel_secret.result)
 }
 
+# https://cloud-study.site  로 접속했을때 
 resource "cloudflare_record" "vmware_dns" {
   zone_id = var.cloudflare_zone_id
   name    = "@"
@@ -17,11 +18,27 @@ resource "cloudflare_record" "vmware_dns" {
   proxied = true
 }
 
+# https://dev.cloud-study.site  로 접속했을때 
+# resource "cloudflare_record" "vmware_dns2" {
+#   zone_id = var.cloudflare_zone_id
+#   name    = "dev"
+#   content = "${cloudflare_tunnel.vmware_tunnel.id}.cfargotunnel.com"
+#   type    = "CNAME"
+#   proxied = true
+# }
+
 resource "cloudflare_tunnel_config" "vmware_config" {
   account_id = cloudflare_tunnel.vmware_tunnel.account_id
   tunnel_id  = cloudflare_tunnel.vmware_tunnel.id
 
   config {
+    # dev. 에 관련된 ingress_rule
+    # ingress_rule {
+    #   hostname = "dev.${var.domain_name}"
+    #   # ALB의 DNS나 Master EC2의 localhost 등으로 트래픽 전달
+    #   service  = "http://<dev 노드의 ip>:80" 
+    # }
+
     ingress_rule {
       hostname = var.domain_name
       # ALB의 DNS나 Master EC2의 localhost 등으로 트래픽 전달
